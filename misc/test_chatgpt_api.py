a4809a8ce3bf4f342fa9c8feb329b1a77e31dd5f
dipayandas97 <dipayan.das2010@gmail.com>
2025-04-10 08:19:32 +0100

    removed keys

--- misc/test_chatgpt_api.py
+++ misc/test_chatgpt_api.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 
 client = OpenAI(
-  api_key="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"
+  api_key=""
 )
 
 completion = client.chat.completions.create(
